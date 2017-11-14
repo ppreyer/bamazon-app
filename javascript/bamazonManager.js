cb2cc62cf134fba92e916b5c782d99f66dfb1287
ppreyer <parker.preyer1@gmail.com>
2017-11-14 14:03:40 -0500

    customer mode complete

---
+++ javascript/bamazonManager.js
@@ -0,0 +1,8 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+// Inquirer Prompt for manager options
+
+// Switch statement for manager selection
+
+// Functions for each case
