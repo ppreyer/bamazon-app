9b4064da351caf9bbab534a4ab83423d03f84043
ppreyer <parker.preyer1@gmail.com>
2017-11-16 19:53:42 -0500

    supervisor-view minus create new department functionality

--- javascript/bamazonManager.js
+++ javascript/bamazonManager.js
@@ -1,13 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
-// Inquirer Prompt for manager options
-// List a set of menu options:
-// View Products for Sale
-// View Low Inventory
-// Add to Inventory
-// Add New Product
-
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -21,19 +14,18 @@ connection.connect(function(err) {
 })
 
 function showManagerMenu() {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "menu",
-      message: "What would you like to check on?",
-      choices: ["View Products for Sale",
+    inquirer.prompt([{
+            type: "list",
+            name: "menu",
+            message: "What would you like to check on?",
+            choices: ["View Products for Sale",
                 "View Low Inventory",
                 "Add to Inventory",
                 "Add New Product"
-               ]
-    }
+            ]
+        }
 
-    ]).then(function(res){
+    ]).then(function(res) {
         checkPromptAnswer(res.menu);
     })
 }
@@ -41,33 +33,33 @@ function showManagerMenu() {
 showManagerMenu();
 
 function checkPromptAnswer(res) {
-  switch(res) {
-    case "View Products for Sale":
-    viewProductsForSale();
-    break;
+    switch (res) {
+        case "View Products for Sale":
+            viewProductsForSale();
+            break;
 
-    case "View Low Inventory":
-    viewLowInventory();
-    break;
+        case "View Low Inventory":
+            viewLowInventory();
+            break;
 
-    case "Add to Inventory":
-    addToInventory();
-    break;
+        case "Add to Inventory":
+            addToInventory();
+            break;
 
-    case "Add New Product":
-    console.log("Add new product");
-    addNewProduct();
-    break
+        case "Add New Product":
+            console.log("Add new product");
+            addNewProduct();
+            break
 
-    default:
-    "Please select an option!"
-  }
+        default:
+            "Please select an option from the menu.";
+    }
 }
 
 function viewProductsForSale() {
-  console.log("View products function now running...");
-  var queryAllProducts = "SELECT * FROM products";
-  connection.query(queryAllProducts, function(err, res) {
+    console.log("View products function now running...");
+    var queryAllProducts = "SELECT * FROM products";
+    connection.query(queryAllProducts, function(err, res) {
         if (err) throw err;
         console.log("\n" + "Here are the current items for sale:" + "\n");
         for (var i = 0; i < res.length; i++) {
@@ -79,14 +71,14 @@ function viewProductsForSale() {
             );
         }
         showManagerMenu();
-  })
+    })
 }
 
 function viewLowInventory() {
-  var queryProductsLessThanFive = "SELECT * FROM products WHERE stock_quantity < 5";
-  connection.query(queryProductsLessThanFive, function(err,res) {
-    if(err) throw err;
-    console.log("\n" + "Here are the current products that have less than five items in their inventory:" + "\n");
+    var queryProductsLessThanFive = "SELECT * FROM products WHERE stock_quantity < 5";
+    connection.query(queryProductsLessThanFive, function(err, res) {
+        if (err) throw err;
+        console.log("\n" + "Here are the current products that have less than five items in their inventory:" + "\n");
         for (var i = 0; i < res.length; i++) {
             var item = res[i];
             console.log(
@@ -96,115 +88,94 @@ function viewLowInventory() {
                 "\n"
             );
         }
-    showManagerMenu();
-  })
+        showManagerMenu();
+    })
 }
 
 function addToInventory() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "product",
-      message: "Please enter the product name you'd like increase the inventory for:"
-    },
-    {
-      type: "input",
-      name: "quantity",
-      message: "Please enter the amount you'd like to add to this product inventory:"
-    }
+    inquirer.prompt([{
+            type: "input",
+            name: "product",
+            message: "Please enter the product name you'd like increase the inventory for:"
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "Please enter the amount you'd like to add to this product inventory:"
+        }
 
     ]).then(function(res) {
-      updateInventoryWithUserQuantity(res);
+        updateInventoryWithUserQuantity(res);
     })
 }
 
 function updateInventoryWithUserQuantity(res) {
-  // SELECT ALL FROM PRODUCTS
-  // check if prompt res === name of product in table
-  // If so then update table with the quantity user inputed
-  var queryAll = "SELECT * FROM PRODUCTS";
-  connection.query(queryAll, function(err, table) {
-    if(err) throw err;
-    var chosenItem = '';
-    for (var i = 0; i < table.length; i++) {
-      var item = table[i];
-      if(item.product_name === res.product) {
-        chosenItem = item;
-      };
-    } 
-      connection.query(
-                    "UPDATE products SET ? WHERE ?", 
-                    [
-                      {
-                        stock_quantity: chosenItem.stock_quantity + parseInt(res.quantity)
-                      },
-                      {
-                        item_id: chosenItem.item_id
-                      }
-                    ], function(err) {
-                    if (err) throw err;
-                    console.log("Product inventory updated.");
-                    showManagerMenu();
-                  }) 
-  })
+    // SELECT ALL FROM PRODUCTS
+    // check if prompt res === name of product in table
+    // If so then update table with the quantity user inputed
+    var queryAll = "SELECT * FROM PRODUCTS";
+    connection.query(queryAll, function(err, table) {
+        if (err) throw err;
+        var chosenItem = '';
+        for (var i = 0; i < table.length; i++) {
+            var item = table[i];
+            if (item.product_name === res.product) {
+                chosenItem = item;
+            };
+        }
+        connection.query(
+            "UPDATE products SET ? WHERE ?", [{
+                    stock_quantity: chosenItem.stock_quantity + parseInt(res.quantity)
+                },
+                {
+                    item_id: chosenItem.item_id
+                }
+            ],
+            function(err) {
+                if (err) throw err;
+                console.log("Product inventory updated.");
+                showManagerMenu();
+            })
+    })
 };
 
 function addNewProduct() {
-  inquirer.prompt([
-      {
-        type: "input",
-        name: "product",
-        message: "What is the name of the product you would like to add?"
-      },
+    inquirer.prompt([{
+            type: "input",
+            name: "product",
+            message: "What is the name of the product you would like to add?"
+        },
 
-      {
-        type: "input",
-        name: "department",
-        message: "Which department should this product be placed?"
-      },
+        {
+            type: "input",
+            name: "department",
+            message: "Which department should this product be placed?"
+        },
 
-      {
-        type: "input",
-        name: "price",
-        message: "What is the price for this item?"
-      },
+        {
+            type: "input",
+            name: "price",
+            message: "What is the price for this item?"
+        },
 
-      {
-        type: "input",
-        name: "quantity",
-        message: "How many items of this product would you like to add to inventory?"
-      },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many items of this product would you like to add to inventory?"
+        },
 
-    ]).then(function(res){
-      var newProduct = {product_name: res.product, department_name: res.department, price: res.price, stock_quantity: res.quantity}
-      var query = connection.query('INSERT INTO products SET ?', newProduct, function(err, res) {
-        if (err) throw err;
-          console.log("Product Added.");
-      showManagerMenu();
-  }) 
- 
-  })
+    ]).then(function(res) {
+        var newProduct = {
+            product_name: res.product,
+            department_name: res.department,
+            price: res.price,
+            stock_quantity: res.quantity
+        }
+        var query = connection.query('INSERT INTO products SET ?', newProduct, function(err, res) {
+            if (err) throw err;
+            console.log("Product Added.");
+            showManagerMenu();
+        })
+
+    })
 }
-
-
-// function showManagerMenu() {
-//   inquirer.prompt([
-//     {
-//       type: "list",
-//       name: "menu",
-//       message: "What would you like to check on?",
-//       choices: ["View Products for Sale",
-//                 "View Low Inventory",
-//                 "Add to Inventory",
-//                 "Add New Product"
-//                ]
-//     }
-
-//     ]).then(function(res){
-//         checkPromptAnswer(res.menu);
-//     })
-// }
-
-// Switch statement for manager selection
-
-// Functions for each case
---
+++ javascript/bamazonSupervisor.js
@@ -0,0 +1,110 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var Table = require('cli-table');
+var table = new Table({
+    head: ['Department ID', 'Department Name', 'Overhead Cost', 'Total Sales', 'Total Profit']
+});
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rolltribe1",
+    database: "bamazonDB",
+    multipleStatements: true
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+})
+
+function showSupervisorView() {
+    inquirer.prompt([
+
+        {
+            type: "list",
+            name: "question",
+            message: "What would you like to do?",
+            choices: ["View Product Sales by Department", "Create New Department"]
+        }
+    ]).then(function(res) {
+        switch (res) {
+            case "View Product Sales by Department":
+                viewProductSalesbyDepartment();
+                break;
+
+            case "Create New Department":
+                createNewDepartment();
+                break;
+            default:
+                "Please select an option from the menu.";
+        }
+    })
+}
+
+// showSupervisorView();
+joinProductTables();
+
+function joinProductTables() {
+    //   SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
+    // FROM Orders
+    // INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
+    var tableJoinQuery = "DROP TABLE IF EXISTS sales; CREATE TABLE sales ( SELECT d.department_id, p.department_name, d.over_head_costs, p.product_sales FROM products p INNER JOIN departments d ON (p.department_name = d.department_name) GROUP BY department_name); SELECT * FROM sales;";
+    connection.query(tableJoinQuery, function(err, res) {
+        if (err) throw err;
+        addTotalProfitColumn(res);
+    })
+}
+
+function addTotalProfitColumn(res) {
+    var addColumn = "ALTER TABLE sales ADD COLUMN total_profit INT(15) AFTER product_sales; SELECT * FROM sales;";
+    connection.query(addColumn, function(err, response) {
+        if (err) throw err
+        for (var i = 1; i < response.length; i++) {
+            var item = response[i];
+            for (var j = 0; j < item.length; j++) {
+                var id = item[j];
+                var totalProfit = item[j].product_sales - item[j].over_head_costs;
+                connection.query(
+                    "UPDATE sales SET ? WHERE ?", [{
+                            total_profit: totalProfit
+                        },
+                        {
+                            department_id: item[j].department_id
+                        }
+                    ],
+
+                    function(err) {
+                        if (err) throw err;
+                    })
+
+                table.push(
+                    [item[j].department_id, item[j].department_name, item[j].over_head_costs]
+                );
+
+            }
+        }
+        console.log(table.toString());
+    })
+
+}
+
+function calculateTotalProfit(res) {
+    for (var i = 1; i < res.length; i++) {
+        var item = res[i];
+        console.log("NEW TABLE", item);
+        var totalProfit = item.product_sales - over_head_costs;
+        connection.query(
+            "UPDATE sales SET ? WHERE ?", [{
+                    total_profit: totalProfit
+                },
+                {
+                    department_id: item.department_id
+                }
+            ],
+            function(err) {
+                if (err) throw err;
+                console.log(t.toString());
+            })
+    }
+}
